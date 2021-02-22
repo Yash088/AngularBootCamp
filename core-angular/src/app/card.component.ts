2d906021cacd56e4a1d3dbe912177b91c947b76a
Yash088 <43039778+Yash088@users.noreply.github.com>
2021-02-22 21:58:39 +0530

    intial commit

---
+++ core-angular/src/app/card.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'my-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.css'],
+})
+export class CardComponent {}
